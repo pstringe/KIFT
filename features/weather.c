b3448bcc76f2593ecb68a1f4ca8e60e10a9a9e51
Steven Song <ssong@e1z3r4p1.42.us.org>
2018-09-14 09:00:16 -0700

    yes

--- features/weather.c
+++ features/weather.c
@@ -6,14 +6,14 @@
 /*   By: ssong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/13 16:51:21 by ssong             #+#    #+#             */
-/*   Updated: 2018/09/13 17:05:19 by ssong            ###   ########.fr       */
+/*   Updated: 2018/09/14 09:00:05 by ssong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <libft.h>
 #include <curl/curl.h>
 
 struct MemoryStruct {
@@ -21,18 +21,17 @@ struct MemoryStruct {
 	size_t size;
 };
 
+// contents is the returned data
+// size is the size of datatype
+// nmemb is number of members
+// *userp is the pointer to user data structure to store.
+
 static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
 	size_t realsize = size * nmemb;
 	struct MemoryStruct *mem = (struct MemoryStruct *)userp;
 
 	mem->memory = realloc(mem->memory, mem->size + realsize + 1);
-	if(mem->memory == NULL) {
-		/* out of memory! */ 
-		printf("not enough memory (realloc returned NULL)\n");
-		return 0;
-	}
-
 	memcpy(&(mem->memory[mem->size]), contents, realsize);
 	mem->size += realsize;
 	mem->memory[mem->size] = 0;
@@ -40,7 +39,7 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
 	return realsize;
 }
 
-int main(void)
+char *weather_curl(void)
 {
 	CURL *curl_handle;
 	CURLcode res;
@@ -50,50 +49,53 @@ int main(void)
 	chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
 	chunk.size = 0;    /* no data at this point */ 
 
-	curl_global_init(CURL_GLOBAL_ALL);
-
-	/* init the curl session */ 
 	curl_handle = curl_easy_init();
-
-	/* specify URL to get */ 
-	curl_easy_setopt(curl_handle, CURLOPT_URL, "http://www.example.com/");
-
-	/* send all data to this function  */ 
+	curl_easy_setopt(curl_handle, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?zip=94555,us&units=imperial&APPID=a84567876c635d5929647ab1879c3122");
 	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-
-	/* we pass our 'chunk' struct to the callback function */ 
 	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-
-	/* some servers don't like requests that are made without a user-agent
-	   field, so we provide one */ 
-	curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-	/* get it! */ 
 	res = curl_easy_perform(curl_handle);
-
-	/* check for errors */ 
-	if(res != CURLE_OK) {
-		fprintf(stderr, "curl_easy_perform() failed: %s\n",
-				curl_easy_strerror(res));
-	}
-	else {
-		/*
-		 * Now, our chunk.memory points to a memory block that is chunk.size
-		 * bytes big and contains the remote file.
-		 *
-		 * Do something nice with it!
-		 */ 
-
-		printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
-	}
-
-	/* cleanup curl stuff */ 
 	curl_easy_cleanup(curl_handle);
+	return (chunk.memory);
+}
 
-	free(chunk.memory);
+char *parse_JSON(char *message)
+{
+	char *response; 
+	char conditions[128];
+	char temperature[128];
+	int i;
 
-	/* we're done with libcurl, so clean it up */ 
-	curl_global_cleanup();
+	i = 0;
+	message = ft_strstr(message, "description");
+	message += 14;
+	while (message[i] != '"')
+		i++;
+	ft_memcpy(conditions, message, i);
+	conditions[i] = 0;
+	
+	message = ft_strstr(message, "temp");
+	message += 6;
+	ft_memcpy(temperature, message, 2);
+	temperature[2] = 0;
+	
+	response = ft_strnew(128);
+	sprintf(response, "Today's temperature is %s degrees farenheit with %s", temperature, conditions);
+	return (response);
+}
 
-	return 0;
+
+char *grab_weather(void)
+{
+	char *data;
+	char *response;
+
+	data = weather_curl();
+	response = parse_JSON(data);
+	return (response);
+}
+
+int main()
+{
+	printf("%s\n", grab_weather());
+	return (0);
 }
