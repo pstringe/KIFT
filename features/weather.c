02f5de86d655b8b785cd0ef1459825c65b3aad4e
Steven Song <ssong@e1z3r5p2.42.us.org>
2018-09-13 18:59:07 -0700

    libft added

--- features/weather.c
+++ features/weather.c
@@ -6,27 +6,94 @@
 /*   By: ssong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/13 16:51:21 by ssong             #+#    #+#             */
-/*   Updated: 2018/09/13 16:55:51 by ssong            ###   ########.fr       */
+/*   Updated: 2018/09/13 17:05:19 by ssong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <curl/curl.h>
+
+struct MemoryStruct {
+	char *memory;
+	size_t size;
+};
+
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+	size_t realsize = size * nmemb;
+	struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+	mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+	if(mem->memory == NULL) {
+		/* out of memory! */ 
+		printf("not enough memory (realloc returned NULL)\n");
+		return 0;
+	}
+
+	memcpy(&(mem->memory[mem->size]), contents, realsize);
+	mem->size += realsize;
+	mem->memory[mem->size] = 0;
+
+	return realsize;
+}
 
 int main(void)
 {
-	CURL *curl;
+	CURL *curl_handle;
 	CURLcode res;
-	char *URL = "http://api.openweathermap.org/data/2.5/weather?zip=94555,us&units=imperial&APPID=a84567876c635d5929647ab1879c3122"
-	
-	curl = curl_easy_init();
-	if(curl)
-	{
-		curl_easy_setopt(curl, CURLOPT_URL, URL);
-		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-		res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
+
+	struct MemoryStruct chunk;
+
+	chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+	chunk.size = 0;    /* no data at this point */ 
+
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	/* init the curl session */ 
+	curl_handle = curl_easy_init();
+
+	/* specify URL to get */ 
+	curl_easy_setopt(curl_handle, CURLOPT_URL, "http://www.example.com/");
+
+	/* send all data to this function  */ 
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+	/* we pass our 'chunk' struct to the callback function */ 
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+	/* some servers don't like requests that are made without a user-agent
+	   field, so we provide one */ 
+	curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+	/* get it! */ 
+	res = curl_easy_perform(curl_handle);
+
+	/* check for errors */ 
+	if(res != CURLE_OK) {
+		fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				curl_easy_strerror(res));
 	}
+	else {
+		/*
+		 * Now, our chunk.memory points to a memory block that is chunk.size
+		 * bytes big and contains the remote file.
+		 *
+		 * Do something nice with it!
+		 */ 
+
+		printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
+	}
+
+	/* cleanup curl stuff */ 
+	curl_easy_cleanup(curl_handle);
+
+	free(chunk.memory);
+
+	/* we're done with libcurl, so clean it up */ 
+	curl_global_cleanup();
+
 	return 0;
 }
