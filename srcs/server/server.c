432c3401b8b98a06f1d081c67110a3c897bf7803
pstringe <stringerpoitier@gmail.com>
2018-12-22 19:03:54 +0000

    documented server.c, partial server_methods.c

--- srcs/server/server.c
+++ srcs/server/server.c
@@ -14,6 +14,9 @@
 
 /*
 **	server command dispatch table
+		The definition for the command object may be found in includes/server/types/s_command.h
+**		TODO: 	allow the first attribute to be an array of strings that way
+			a command may be called via multiple names
 */
 
 struct s_command g_cmds[NO_OF_CMDS] = {
@@ -24,6 +27,26 @@ struct s_command g_cmds[NO_OF_CMDS] = {
 
 /*
 **	initialize server object
+** 		s->port: the port number the server listens for the request on
+**
+**		history_init:	initializes the the properties and methods 
+**				needed for the history object to function
+**
+**		establish_connection: 	found in server_methods.c, the function that 
+**					allows the server to connect to the client
+**					on the established port.
+**
+**		listening:	found in server_methods.c, the function which listens for
+**				information written to the socket descriptor
+
+**		s->cmds: 	a global variable representing the list of functions that
+**				may be called by the server accompanied by the names by which
+**				they may be called from the client. Implemented via an array 
+**				of structs of type s_cmd, the type definition
+**
+**		dispatch:	This function reads the request sent by the server
+**
+**		respond:	This function which relays server output to the client via the socket descriptor
 */
 
 void	server_init(t_server *s, int port)
@@ -36,6 +59,15 @@ void	server_init(t_server *s, int port)
 	s->dispatch = dispatch;
 	s->respond = respond;
 }
+
+/*
+** The main function which starts the server contains three functions:
+**	server_init: initializes the values and functions vital to the operation of the server
+**	server.connect: points to establish_connection() in server_methods.c
+**			accepts a client device's request to connect
+**	server.listen:	points to listening() in server_methods.c
+**			listens for bytes written the the socket descriptionfrom the client device	
+*/
 
 int		main(int argc, char **argv)
 {
--- srcs/server/server_methods.c
+++ srcs/server/server_methods.c
@@ -23,15 +23,20 @@ int		establish_connection(t_server *server, int argc, char **argv)
 		perror("please specify port to listen on");
 		return (-1);
 	}
+
+	//initialize a socket
 	if (!(server->s_sock = socket(AF_INET, SOCK_STREAM, 0)))
 	{
 		perror("failed to get socket descriptor");
 		return (-1);
 	}
+	//specify address type
 	server->addr.sin_family = AF_INET;
+	
 	server->addr.sin_addr.s_addr = INADDR_ANY;
 	server->addr.sin_port = htons(server->port);
 	server->addr_len = sizeof(server->addr);
+
 	if (bind(server->s_sock, (struct sockaddr*)&(server->addr), 
 				(socklen_t)sizeof(server->addr)) < 0)
 	{
@@ -58,7 +63,7 @@ void	respond(t_server *server, char *msg, size_t size)
 }
 
 /*
-**	this is the called for the server to listen continuosly on the read socket and
+**	This is the called for the server to listen continuosly on the read socket and
 **	having registered a valid command, exit.
 */
 
@@ -82,28 +87,65 @@ void	listening(t_server *server)
 
 /*
 ** server dispatch
+**	client_input:	The buffer that is used to read from the socket descriptor
+**	SOCK_BUF_SIZE:	The size of the buffer
+**	server->request:	An object, attatched to the server, never reallocated,
+**				only updated with new values:
+**				name: 	the label for the command that is triggered by the client input
+**					(since eventually there will be more than one phrase that can trigger the same command)
+**					
+**					TODO: come to think of it, this means I will have to update the s_cmd struct so that there is,
+**				       	(1), an array of strings representing the possible triggers for a command, (2) the lable for
+**					the command as it will be relected in server history, (3) a pointer to a function for the 
+**					command itself.	
+**				
+**				action: A pointer to a function for the command that was executed, taken from the cmd object being accessed
+**					It will be useful to save this incase we want to have a function that allows command repetition based
+**					on history.
+**					
+**
 */
 
 int		dispatch(t_server *server)
 {
 	int 	i;
 	char 	client_input[SOCK_BUF_SIZE];
-
+	
+	//make sure this buffer is clear before using it
 	ft_bzero(client_input, SOCK_BUF_SIZE);
+	
+	//copy over the text from the request object into the client input variable
 	ft_memcpy(client_input, server->request.text, server->request.size);  
 	i = -1;
+	
+	//itterate over each command in the dispatch table
 	while (server->cmds[++i].name)
 	{
+		//try to match the client input to a command via the name attribute
 		if (!ft_strncmp(client_input, server->cmds[i].name, ft_strlen(client_input)))
 		{
+			//update the name based on the the command that was resolved
 			server->request.command.name = server->cmds[i].name;
+
+			//update the action 
 			server->request.command.action = server->cmds[i].action;
+			
+			//execute the command
 			server->cmds[i].action(server);
+
+			//Not sure why I wrote this condition. the first command in the dispatch table is quit.
+			//It seems that, at some point, I decided I didn't want the quit command to log in history
+			//perhaps this made it easier to debug the history functions since I didn't have to end up
+			//with extra quit commands everytime I terminated the server.
+			//TODO: Contemplate removing this condition, make sure it doesn't fuck anything up
 			if (i)
+				//update hitory with the last command executed
 				server->history.update(server);
 			return (1);
 		}
 	}
+	//If none of the commands can be matched based on the input, set the resquests command name to null and update
+	//the history anyway, indicating the input did not resilve to any command
 	server->request.command.name = NULL;
 	server->history.update(server);
 	return (0);
