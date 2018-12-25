adfe24fd24199cca61039a2b95281b44948b143d
Poitier Stringer <pstringe@e1z3r2p22.42.us.org>
2018-12-25 05:24:01 -0800

    down to zero errors on multi-client echo server

--- srcs/server/server_methods.c
+++ srcs/server/server_methods.c
@@ -6,11 +6,12 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/05 19:49:30 by pstringe          #+#    #+#             */
-/*   Updated: 2018/12/24 17:11:34 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/12/25 05:23:15 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
+#include <errno.h>
 
 /*
 **	This function is called to establish a connection with the client machine
@@ -32,7 +33,7 @@ int		establish_connection(t_server *server, int argc)
 	}
 
 	//set master socket to allow multiple connections
-	if (setsckopt(server->m_sock, SOL_SOCKET, SO_REUSEADDR, \
+	if (setsockopt(server->m_sock, SOL_SOCKET, SO_REUSEADDR, \
 				(char *)&(server->opt), sizeof(server->opt)) < 0 )
 	{
 		perror("setsocketopt");
@@ -45,8 +46,7 @@ int		establish_connection(t_server *server, int argc)
 	server->addr_len = sizeof(server->addr);
 	
 	//bind port to master socket
-	if (bind(server->m_sock, (struct sockaddr*)&(server->addr), 
-				(socklen_t)sizeof(server->addr)) < 0)
+	if (bind(server->m_sock, (struct sockaddr*)&(server->addr), (socklen_t)sizeof(server->addr)) < 0)
 	{
 		perror("faild to bind port to address");
 		return (-1);
@@ -79,6 +79,101 @@ void	respond(t_server *server, char *msg, size_t size)
 
 void	listening(t_server *server)
 {
+	int	i;
+	int	ret;
+	int	highest;
+	int	n_sock;
+	char buf[1025];
+	char *message;
+
+	message = "echo server online\n";
+	while(1)
+	{
+		//clear socket set
+		FD_ZERO(&(server->readfds));
+		
+		//add master socket to set
+		FD_SET(server->m_sock, &(server->readfds));
+
+		//add child sockets to set
+		i = -1;
+		while (++i < MAX_CLIENTS)
+		{
+			//socket descriptor
+			server->sd = server->client_sockets[i];
+			
+			//add to list if valid
+			if (server->sd > 0)
+				FD_SET(server->sd, &(server->readfds));
+
+			//get highest file descripter
+			if (server->sd > highest)
+				highest = server->sd;
+		}
+
+		//wait for activity on one of the sockets
+		server->activity = select(highest + 1, &(server->readfds), NULL, NULL, NULL);
+		if (server->activity < 0 && errno != EINTR)
+			printf("selcect error");
+
+		//handle incoming connections
+		if (FD_ISSET(server->m_sock, &(server->readfds)))
+		{
+			if ((n_sock = accept(server->m_sock, (struct sockaddr *)&(server->addr), (socklen_t*)&(server->addr_len))) < 0)
+			{
+				perror("accept");
+				return ;
+			}
+		}
+		printf("New Connection\nsocketfd: %d,\nip: %s,\nport: %d\n", n_sock, inet_ntoa(server->addr.sin_addr), ntohs(server->addr.sin_port));
+		
+		//send new connection message
+		if (send(n_sock, message, ft_strlen(message), 0) != (ssize_t)ft_strlen(message))
+			perror("send");
+		ft_putendl("welcome sent success");
+		
+		//add socket to array
+		i = -1;
+		while (++i)
+		{
+			if (!server->client_sockets[i])
+			{
+				server->client_sockets[i] = n_sock;
+				ft_printf("Adding to list of sockets");
+				break;
+			}
+		}
+	}
+	
+	//handle IO operation
+	i = -1;
+	while (++i < MAX_CLIENTS)
+	{
+		server->sd = server->client_sockets[i];
+		if (FD_ISSET(server->sd, &(server->readfds)))
+		{
+			//check if was close and read message
+			if ((ret = read(server->sd, buf, 1024)) == 0)
+			{
+				//get disconnected client details
+				getpeername(server->sd, (struct sockaddr*)&server->addr, (socklen_t*)&server->addr_len);
+				ft_printf("Host disconnected, ip is: %s, port %d\n", inet_ntoa(server->addr.sin_addr), ntohs(server->addr.sin_port));
+
+				//Close socket and zero it out
+				close(server->sd);
+				server->client_sockets[i] = 0;
+			}
+		}
+
+		//Echo the message that came in
+		else
+		{
+			//terminate the buffer
+			buf[ret] = '\0';
+			send(server->sd, buf, ft_strlen(buf), 0);
+		}
+	}
+	/*
 	server->c_sock = accept(server->s_sock, (struct sockaddr*)&(server->addr), (socklen_t*)&(server->addr_len));
 	while (server->listening)
 	{
@@ -93,6 +188,8 @@ void	listening(t_server *server)
 		}
 	}
 	close(server->c_sock);
+	*/
+
 }
 
 /*
