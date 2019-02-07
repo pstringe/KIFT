e1701cdb314ed52701bf3d31afd2f3187fd3d4f2
Jamie Dawson <jadawson@e1z3r3p23.42.us.org>
2019-02-07 15:44:24 -0800

    alarm accepts 2 or 3 arguements and converts time. Need to connect to voice server

--- alarm/alarm.c
+++ alarm/alarm.c
@@ -4,53 +4,21 @@
 #include <string.h>
 #include <time.h>
 
-
-int		pm_handle(int hr) //REMOVE AND JUST ADD 12 to NUMBER FUCK
-{
-	int built_in_hour = 0;
-
-	if (hr == 1)
-		built_in_hour = 13;
-	if (hr == 2)
-		built_in_hour = 14;
-	if (hr == 3)
-		built_in_hour = 15;
-	if (hr == 4)
-		built_in_hour = 16;
-	if (hr == 5)
-		built_in_hour = 17;
-	if (hr == 6)
-		built_in_hour = 18;
-	if (hr == 7)
-		built_in_hour = 19;
-	if (hr == 8)
-		built_in_hour = 20;
-	if (hr == 9)
-		built_in_hour = 21;
-	if (hr == 10)
-		built_in_hour = 22;
-	if (hr == 11)
-		built_in_hour = 23;
-	return (built_in_hour);
-}
-
 int		convert_hour(char *hr, char *am_pm)
 {
 	int con_hour = atoi(hr);
 	int	send_am_or_pm_hour = 0;
 
-	printf("%i converted time\n", con_hour);
+	printf("%i converted time\n", con_hour); //TESTING REMOVE
 
 	if (strcmp(am_pm, "pm") == 0)
-		send_am_or_pm_hour = pm_handle(con_hour);
+		send_am_or_pm_hour = con_hour + 12;
 	if (strcmp(am_pm, "am") == 0)
 		send_am_or_pm_hour = con_hour;
-	printf("found am hour (line48) %i\n", con_hour);
+	printf("found am hour (line48) %i\n", con_hour); //TESTING REMOVE
 
-	return(con_hour);
+	return(send_am_or_pm_hour);
 }
-
-
 
 int		kift_alarm(int hr, int min)
 {
@@ -72,17 +40,16 @@ int		kift_alarm(int hr, int min)
 
 		sleep (2);
 		system("clear");
-		printf("%i not there\n", myTime->tm_min);
-		printf("%i not there\n", myTime->tm_hour);
+		printf("%i not there\n", myTime->tm_min); //TESTING REMOVE
+		printf("%i not there\n", myTime->tm_hour);//TESTING REMOVE
 	}
 	if (min == myTime->tm_min && hr == myTime->tm_hour)
 	{
-		printf("%i is the min AT END\n", myTime->tm_min);
-		printf("%i is the hour AT END\n", myTime->tm_hour);
+		printf("%i is the min AT END\n", myTime->tm_min);//TESTING REMOVE
+		printf("%i is the hour AT END\n", myTime->tm_hour);//TESTING REMOVE
 	}
 	return (0);
 }
-
 
 int		main(int argc, char **argv)
 {
@@ -90,11 +57,19 @@ int		main(int argc, char **argv)
 	int min = 0;
 
 	if (argc == 4)
+	{
 		hr = convert_hour(argv[1], argv[3]); //hour + am/pm
-	min = atoi(argv[2]);
-	printf("%i min in main (line95)\n", min);
-	printf("%i hour in main (line 95\n", hr);
-	//kift_alarm(hr, min); //hour, minute.
+		min = atoi(argv[2]);
+	}
+	if (argc == 3) //just saying 3pm, 5pm, etc. No minute given.
+	{
+		int hr = convert_hour(argv[1], argv[2]); //hour + am/pm
+		min = 0;
+	}
+	
+	printf("%i min in main (line95)\n", min); //TESTING REMOVE
+	printf("%i hour in main (line 95)\n", hr);//TESTING REMOVE
+	kift_alarm(hr, min); //hour, minute.
 }
 
 
