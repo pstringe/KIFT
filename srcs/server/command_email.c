e8afa33f6eebbc0cce2009f525cb90db9d9772b7
Poitier Stringer <pstringe@e1z3r4p1.42.us.org>
2019-03-03 01:46:15 -0800

    fixed email prompt

--- srcs/server/command_email.c
+++ srcs/server/command_email.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 21:47:23 by pstringe          #+#    #+#             */
-/*   Updated: 2019/02/18 15:39:41 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/03 01:43:01 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ void cmd_email(t_server *s)
 	char *email = "42kift@gmail.com ";
 	char *psswd = "42Kiftkift";
 
-	dst = ft_atoi(prompt_request(s, s->l_sock, "Who would you like to send an email to? You can say:\n \
-			1, for Jaimie,\n \
-			2, for Poitier,\n \
-			3, for Dylan,\n \
-			4, for Malik.\n \
+	dst = ft_atoi(prompt_request(s, s->l_sock, "Who would you like to send an email to? You can say: \
+			1, for Jaimie, \
+			2, for Poitier, \
+			3, for Dylan, \
+			4, for Malik. \
 	").text);
 	
 	if (dst == 1)
