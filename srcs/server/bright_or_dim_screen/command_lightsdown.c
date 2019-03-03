4f08ba0c7fdc505430fbb9938ba9b6435bf691a2
Poitier Stringer <pstringe@e1z3r4p1.42.us.org>
2019-03-03 01:34:03 -0800

    fixed the blocking probem by having the server issue a response

--- srcs/server/bright_or_dim_screen/command_lightsdown.c
+++ srcs/server/bright_or_dim_screen/command_lightsdown.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 19:38:22 by pstringe          #+#    #+#             */
-/*   Updated: 2019/02/18 19:46:28 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/03 01:29:46 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	cmd_lightsdown(t_server *s)
 {
-	(void)s;
 	system("osascript srcs/server/bright_or_dim_screen/make_dim.script");
+	s->respond(s, "lights down", 11);
 }
 
--- srcs/server/bright_or_dim_screen/command_lightsup.c
+++ srcs/server/bright_or_dim_screen/command_lightsup.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 19:37:02 by pstringe          #+#    #+#             */
-/*   Updated: 2019/02/18 19:45:41 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/03 01:28:16 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	cmd_lightsup(t_server *s)
 {
-	(void)s;
 	system("osascript srcs/server/bright_or_dim_screen/make_bright.script");
+	s->respond(s, "lights up", 9);
 }
 
