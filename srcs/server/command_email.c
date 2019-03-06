5d36b9489018359e75e4c15d7741fa37dde66f90
Jamie Dawson <jadawson@e1z3r2p8.42.us.org>
2019-03-05 21:11:54 -0800

    fixed a few norm errors but there are still too many

--- srcs/server/command_email.c
+++ srcs/server/command_email.c
@@ -6,21 +6,21 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 21:47:23 by pstringe          #+#    #+#             */
-/*   Updated: 2019/03/04 08:42:29 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/05 21:11:37 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-void cmd_email(t_server *s)
+void		cmd_email(t_server *s)
 {
-	char *cmd[512];
-	char *destination;
-	int	 dst;
-	char *script = "python srcs/server/cmd_email.py ";
-	char *msg = "\"Congratualations you just recieved an email from kift\" ";
-	char *email = "42kift@gmail.com ";
-	char *psswd = "42Kiftkift";
+	char	*cmd[512];
+	char	*destination;
+	int		dst;
+	char	*script = "python srcs/server/cmd_email.py ";
+	char	*msg = "\"Congratualations you just recieved an email from kift\" ";
+	char	*email = "42kift@gmail.com ";
+	char	*psswd = "42Kiftkift";
 
 	dst = ft_atoi(prompt_request(s, s->l_sock, "Who would you like to send an email to? You can say: \
 			1, for Jaimie, \
@@ -28,7 +28,6 @@ void cmd_email(t_server *s)
 			3, for Dylan, \
 			4, for Malik. \
 	").text);
-	
 	if (dst == 1)
 		destination = ft_strdup("jadawson@student.42.us.org ");
 	else if (dst == 2)
@@ -37,15 +36,12 @@ void cmd_email(t_server *s)
 		destination = ft_strdup("dysotoma@student.42.us.org ");
 	else if (dst == 4)
 		destination = ft_strdup("maljean@student.42.us.org ");
-	
-
 	ft_bzero(cmd, 512);
 	ft_strlcat((char*)cmd, script, ft_strlen(script));
 	ft_strlcat((char*)cmd, msg, ft_strlen(msg));
 	ft_strlcat((char*)cmd, destination, ft_strlen(destination));
 	ft_strlcat((char*)cmd, email, ft_strlen(email));
 	ft_strlcat((char*)cmd, psswd, ft_strlen(psswd));
-	
 	ft_printf("email string: %s\n", cmd);
 	system((const char*)cmd);
 	s->respond(s, "Email has been sent\n", 21);
