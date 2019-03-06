da750927fcc2effc3391015e74a75c33466e2bef
Jamie Dawson <jadawson@e1z3r2p8.42.us.org>
2019-03-05 22:26:42 -0800

    fixed norm errors

--- srcs/server/bright_or_dim_screen/command_lightsdown.c
+++ srcs/server/bright_or_dim_screen/command_lightsdown.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 19:38:22 by pstringe          #+#    #+#             */
-/*   Updated: 2019/03/03 01:29:46 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/05 22:26:02 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,3 @@ void	cmd_lightsdown(t_server *s)
 	system("osascript srcs/server/bright_or_dim_screen/make_dim.script");
 	s->respond(s, "lights down", 11);
 }
-
--- srcs/server/bright_or_dim_screen/command_lightsup.c
+++ srcs/server/bright_or_dim_screen/command_lightsup.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 19:37:02 by pstringe          #+#    #+#             */
-/*   Updated: 2019/03/03 01:28:16 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/05 22:26:19 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,3 @@ void	cmd_lightsup(t_server *s)
 	system("osascript srcs/server/bright_or_dim_screen/make_bright.script");
 	s->respond(s, "lights up", 9);
 }
-
