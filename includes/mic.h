1f80cc49de1bae95947d547f406abd1feeeb4afb
Jamie Dawson <jadawson@e1z3r2p8.42.us.org>
2019-03-05 19:25:12 -0800

    fixed norm error

--- includes/mic.h
+++ includes/mic.h
@@ -6,12 +6,15 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 17:45:14 by jadawson          #+#    #+#             */
-/*   Updated: 2019/03/05 17:46:27 by jadawson         ###   ########.fr       */
+/*   Updated: 2019/03/05 19:24:35 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pocketsphinx.h>
-#include <sphinxbase/ad.h>
-#include <sphinxbase/err.h>
+#ifndef MIC_H
+# define MIC_H
+# include <pocketsphinx.h>
+# include <sphinxbase/ad.h>
+# include <sphinxbase/err.h>
 
 const char *recognize_from_microphone();
+#endif
