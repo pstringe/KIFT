9e3375769baf1463f83149623571ef75d1242dba
Jamie Dawson <jadawson@e1z3r2p8.42.us.org>
2019-03-05 20:46:41 -0800

    fixed norm spacing

--- includes/types/s_request.h
+++ includes/types/s_request.h
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/15 14:51:14 by pstringe          #+#    #+#             */
-/*   Updated: 2019/03/05 18:03:27 by jadawson         ###   ########.fr       */
+/*   Updated: 2019/03/05 20:46:22 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 
 typedef struct			s_request
 {
-	int			type;
-	char			text[BUF_SIZE];
+	int					type;
+	char				text[BUF_SIZE];
 	struct s_command	command;
 	int					size;
 }						t_request;
