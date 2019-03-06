cdfa866cba772ac6042c04306ef8246286575864
Jamie Dawson <jadawson@e1z3r2p8.42.us.org>
2019-03-05 20:45:49 -0800

    updated norm spacing

--- includes/types/s_user.h
+++ includes/types/s_user.h
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 14:01:50 by pstringe          #+#    #+#             */
-/*   Updated: 2019/02/24 18:28:59 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/05 20:45:26 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 
 typedef struct	s_user
 {
-	char 	name[256];
-	int		port;
-	int		socket;
-	char	*address;
+	char		name[256];
+	int			port;
+	int			socket;
+	char		*address;
 }				t_user;
 #endif
