e4d918da187f0c78dbcbb4be2518035832919b75
Jamie Dawson <jadawson@e1z3r2p8.42.us.org>
2019-03-05 21:19:50 -0800

    updated tab norm error

--- srcs/server/command_search.c
+++ srcs/server/command_search.c
@@ -6,14 +6,14 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 20:52:37 by pstringe          #+#    #+#             */
-/*   Updated: 2019/02/17 21:02:01 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/05 21:19:17 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-void cmd_search(t_server *s)
+void	cmd_search(t_server *s)
 {
 	s->respond(s, "search initiated", 17);
-    system("open -a \"Google Chrome\" https://www.google.com/search\\\?q\\=");
+	system("open -a \"Google Chrome\" https://www.google.com/search\\\?q\\=");
 }
