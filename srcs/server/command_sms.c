886172ed42eb3e549c76a1e38be64d2967c55c9b
Poitier Stringer <pstringe@e1z2r5p8.42.us.org>
2019-03-07 13:14:08 -0800

    fixed sms

--- srcs/server/command_sms.c
+++ srcs/server/command_sms.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 14:53:31 by pstringe          #+#    #+#             */
-/*   Updated: 2019/03/05 21:26:41 by jadawson         ###   ########.fr       */
+/*   Updated: 2019/03/07 13:12:49 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	cmd_sms(t_server *s)
 	char	*script;
 	char	*body;
 
-	no = "12094908409";
+	no = "2094908409 ";
 	script = "python srcs/server/sms.py ";
 	body = "Congratulations! You just recieved an sms from KIFT";
 	ft_bzero(cmd, 512);
