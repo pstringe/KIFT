62e16752a09d8c8c32f8e5bd00c683b718d4c0b6
Jamie Dawson <jadawson@e1z2r3p36.42.us.org>
2019-03-06 20:16:21 -0800

    applied norm to alarm

--- srcs/server/alarm/alarm.c
+++ srcs/server/alarm/alarm.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   alarm.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/06 19:31:43 by jadawson          #+#    #+#             */
+/*   Updated: 2019/03/06 20:15:35 by jadawson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,77 +19,63 @@
 
 int		convert_hour(char *hr, char *am_pm)
 {
-	int con_hour = atoi(hr);
-	int	send_am_or_pm_hour = 0;
+	int con_hour;
+	int	send_am_or_pm_hour;
 
-	printf("%i converted time\n", con_hour); //TESTING REMOVE
-
-	if (strcmp(am_pm, "pm") == 0)
+	con_hour = atoi(hr);
+	send_am_or_pm_hour = 0;
+	if (strcmp(am_pm, "PM") == 0)
 		send_am_or_pm_hour = con_hour + 12;
-	if (strcmp(am_pm, "am") == 0)
+	if (strcmp(am_pm, "AM") == 0)
 		send_am_or_pm_hour = con_hour;
-	printf("found am hour (line48) %i\n", con_hour); //TESTING REMOVE
-
-	return(send_am_or_pm_hour);
+	ft_printf("found am hour (line48) %i\n", con_hour);
+	return (send_am_or_pm_hour);
 }
 
 int		kift_alarm(int hr, int min)
 {
-	time_t currentTime;
-	time(&currentTime);
+	time_t		current_time;
+	struct tm	*my_time;
 
-
-	struct tm *myTime =localtime(&currentTime);
-//	printf("%i\n", myTime->tm_mon);
-
-	printf("%i is the min BEGINNING\n", myTime->tm_min);
-	printf("%i is the hour BEGINNING\n", myTime->tm_hour);
-	sleep(2);
-	while (hr != myTime->tm_hour || min != myTime->tm_min)
+	my_time = localtime(&current_time);
+	time(&current_time);
+	while (hr != my_time->tm_hour || min != my_time->tm_min)
 	{
-		time_t currentTime;
-		time(&currentTime);
-		struct tm *myTime =localtime(&currentTime);
-
-		sleep (2);
+		time(&current_time);
+		my_time = localtime(&current_time);
+		sleep(2);
 		system("clear");
-		printf("%i not there\n", myTime->tm_min); //TESTING REMOVE
-		printf("%i not there\n", myTime->tm_hour);//TESTING REMOVE
+		ft_printf("%i not there\n", my_time->tm_min);
+		ft_printf("%i not there\n", my_time->tm_hour);
 	}
-	if (min == myTime->tm_min && hr == myTime->tm_hour)
+	if (min == my_time->tm_min && hr == my_time->tm_hour)
 	{
-		printf("%i is the min AT END\n", myTime->tm_min);//TESTING REMOVE
-		printf("%i is the hour AT END\n", myTime->tm_hour);//TESTING REMOVE
+		ft_printf("%i is the min AT END\n", my_time->tm_min);
+		ft_printf("%i is the hour AT END\n", my_time->tm_hour);
 	}
 	return (0);
 }
 
 void	cmd_alarm(t_server *s)
 {
-	int hr = 0;
-	int min = 0;
-	char *hour = NULL;
-	char *minute = NULL;
-	char *am_pm = NULL;
-	
-	hour = ft_strdup(prompt_request(s, s->l_sock, "What time? Please tell me the hour.").text);
+	int		hr;
+	int		min;
+	char	*hour;
+	char	*minute;
+	char	*am_pm;
+
+	hour = ft_strdup(prompt_request(s, s->l_sock,
+				"What time? Please tell me the hour.").text);
 	ft_printf("test hour: %s\n", hour);
-	minute = ft_strdup(prompt_request(s, s->l_sock, "Alright, Please tell me the minute").text);
+	minute = ft_strdup(prompt_request(s, s->l_sock,
+				"Alright, Please tell me the minute").text);
 	ft_printf("test minute: %s\n", minute);
-	am_pm = ft_strdup(prompt_request(s, s->l_sock, "Please tell me, am or pm?").text);
-	hr = convert_hour(hour, am_pm); //hour + am/pm
+	am_pm = ft_strdup(prompt_request(s, s->l_sock,
+				"Please tell me, am or pm?").text);
+	hr = convert_hour(hour, am_pm);
 	min = ft_atoi(minute);
 	free(hour);
 	free(minute);
-	kift_alarm(hr, min); //hour, minute.
+	kift_alarm(hr, min);
 	s->respond(s, "Ring Ring Ring", 15);
 }
-
-
-/*
-currently works with hours and minutes.
-Now I just need to convert arguements for the hours.
-
-int	tm_min;   minutes range 0 to 59
-int	tm_hour;  hours range 0 to 23
-*/
