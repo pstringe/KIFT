dcabf47b9b7114f6cb249df90574dc771f59397d
Poitier Stringer <pstringe@e1z2r3p34.42.us.org>
2019-03-06 18:48:06 -0800

    norme team

--- srcs/server/command_team.c
+++ srcs/server/command_team.c
@@ -6,13 +6,13 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 10:59:23 by jadawson          #+#    #+#             */
-/*   Updated: 2019/03/04 21:12:11 by jadawson         ###   ########.fr       */
+/*   Updated: 2019/03/06 18:46:19 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-void cmd_team(t_server *s)
+void	cmd_team(t_server *s)
 {
 	s->respond(s, "Displaying team", 15);
 	system("open https://www.flickr.com/photos/162616625@N08/33410045108/in\
