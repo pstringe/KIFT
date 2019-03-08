0bc7f2e709ba68b75463e5b56257898c56ebcc48
Poitier Stringer <pstringe@e1z2r5p8.42.us.org>
2019-03-07 16:11:50 -0800

    improved number recognition, updated alarm to only take minutes

--- srcs/server/alarm/alarm.c
+++ srcs/server/alarm/alarm.c
@@ -6,7 +6,7 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/06 19:31:43 by jadawson          #+#    #+#             */
-/*   Updated: 2019/03/07 14:53:10 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/03/07 16:10:10 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,72 +17,40 @@
 #include <time.h>
 #include "server.h"
 
-int		convert_hour(char *hr, char *am_pm)
-{
-	int con_hour;
-	int	send_am_or_pm_hour;
-
-	con_hour = atoi(hr);
-	send_am_or_pm_hour = 0;
-	if (strcmp(am_pm, "PM") == 0)
-		send_am_or_pm_hour = con_hour + 12;
-	if (strcmp(am_pm, "AM") == 0)
-		send_am_or_pm_hour = con_hour;
-	ft_printf("found am hour (line48) %i\n", con_hour);
-	return (send_am_or_pm_hour);
-}
-
-int		kift_alarm(/*int hr, int min*/)
+int		kift_alarm(int t)
 {
 	int			hr;
 	int			min;
-	int 		t;
 	time_t		current_time;
-	struct tm	*my_time;
+	struct tm	*m_t;
 
 	t = 1;
 	time(&current_time);
-	my_time = localtime(&current_time);
-	hr = my_time->tm_min + t >= 60 ? my_time->tm_hour + 1 : my_time->tm_hour;
-	min = my_time->tm_min + t > 60 ? my_time->tm_min + t - 60 : my_time->tm_min + t;
-	while (hr != my_time->tm_hour || min != my_time->tm_min)
+	m_t = localtime(&current_time);
+	hr = m_t->tm_min + t >= 60 ? m_t->tm_hour + 1 : m_t->tm_hour;
+	min = m_t->tm_min + t > 60 ? m_t->tm_min + t - 60 : m_t->tm_min + t;
+	while ((hr != m_t->tm_hour || min != m_t->tm_min) &&
+			time(&current_time) && (m_t = localtime(&current_time)))
 	{
-		time(&current_time);
-		my_time = localtime(&current_time);
 		sleep(2);
 		system("clear");
-		ft_printf("%i not there\n", my_time->tm_min);
-		ft_printf("%i not there\n", my_time->tm_hour);
+		ft_printf("%i not there\n", m_t->tm_min);
+		ft_printf("%i not there\n", m_t->tm_hour);
 	}
-	if (min == my_time->tm_min && hr == my_time->tm_hour)
+	if (min == m_t->tm_min && hr == m_t->tm_hour)
 	{
-		ft_printf("%i is the min AT END\n", my_time->tm_min);
-		ft_printf("%i is the hour AT END\n", my_time->tm_hour);
+		ft_printf("%i is the min AT END\n", m_t->tm_min);
+		ft_printf("%i is the hour AT END\n", m_t->tm_hour);
 	}
 	return (0);
 }
 
 void	cmd_alarm(t_server *s)
 {
-	/*
-	char	*hour;
-	char	*minute;
-	char	*am_pm;
-	
-	hour = ft_strdup(prompt_request(s, s->l_sock,
-				"What time? Please tell me the hour.").text);
-	ft_printf("test hour: %s\n", hour);
-	minute = ft_strdup(prompt_request(s, s->l_sock,
-				"Alright, Please tell me the minute").text);
-	ft_printf("test minute: %s\n", minute);
-	am_pm = ft_strdup(prompt_request(s, s->l_sock,
-				"Please tell me, am or pm?").text);
-	hr = convert_hour(hour, am_pm);
-	min = ft_atoi(minute);
-	free(hour);
-	free(minute);
-	kift_alarm(hr, min);
-	*/
-	kift_alarm();
+	int t;
+
+	t = ft_atoi(prompt_request(s, s->l_sock, 
+				"Please say the number of minutes").text);
+	kift_alarm(t);
 	s->respond(s, "Ring Ring Ring", 15);
 }
